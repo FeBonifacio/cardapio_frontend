f6c269b0e6072921570f463f190c0f5dc791cc88
Fernando <fe.bonifacio@hotmail.com>
2024-01-15 16:52:27 -0300

    deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@
     let cookies = parseCookies(ctx);
 
     const api = axios.create({
-        baseURL: 'http://localhost:3333',
+        baseURL: 'https://currency-seven-woad.vercel.app/',
         headers: {
         Authorization: `Bearer ${cookies['@nextauth.token']}`
         }
